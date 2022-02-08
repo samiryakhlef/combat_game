6c46bd3ce02c136b8e95120bcb1aaaddfc58414b
samiryakhlef <samir.yakhlef@gmail.com>
2022-02-08 11:53:48 +0100

    class personnage + hero

---
+++ src/Heros.php
@@ -0,0 +1,20 @@
+<?php
+
+class Personnage {
+
+    protected $vie;
+    protected $degat;
+    public $bonus_vie;
+    public $bonus_armure;
+
+
+    protected function attaque_heros($cible) {
+       $cible->vie = "15" ;
+
+    }
+    protected function bonus_vie(){
+
+    }
+
+
+} 
---
+++ src/Personnage.php
@@ -0,0 +1,18 @@
+<?php
+namespace App;
+
+use Personnage;
+
+class Heros extends Personnage{
+    public $vie;
+    public $degat;
+
+
+   public function __construct()
+        {
+            $this->vie = "200";
+            $this->degat = "15";
+            $this-> bonus_vie = "50" ;
+            $this-> bonus_armure = "20";
+        }
+}
